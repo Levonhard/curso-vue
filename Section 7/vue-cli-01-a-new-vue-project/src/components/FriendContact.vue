4fba60b1e79437eb04186f36bb58a36dd4605135
LEONARDO LOPES RODRIGUES <llrodrigues@oabrs>
2023-02-22 14:55:41 -0300

    Rename folders to sections numbers instead of sections names

--- Section 7/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
+++ Section 7/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -3,8 +3,12 @@
         <h2>{{ friend.name }}</h2>
         <button @click="toggleDetails()">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
         <ul v-if="detailsAreVisible">
-            <li><strong>Phone:</strong> {{ friend.phone }}</li>
-            <li><strong>Email:</strong> {{ friend.email }}</li>
+            <li>
+                <strong>Phone:</strong> {{ friend.phone }}
+            </li>
+            <li>
+                <strong>Email:</strong> {{ friend.email }}
+            </li>
         </ul>
     </li>
 </template>
