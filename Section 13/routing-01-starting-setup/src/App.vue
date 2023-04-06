e66f9ea498d2ac543330dbc566418cccb8e81a0a
Leonardo <levonhard7@gmail.com>
2023-04-06 17:57:19 -0300

    Add more content to section 13

--- Section 13/routing-01-starting-setup/src/App.vue
+++ Section 13/routing-01-starting-setup/src/App.vue
@@ -3,7 +3,11 @@
   <main>
     <!-- <component :is="activePage"></component> -->
     <router-view></router-view>
+
   </main>
+  <footer>
+      <router-view name="footer"></router-view>
+  </footer>
 </template>
 
 <script>
